9167a671eae113ab2959f4ba7dcc0ff45139819f
Lorenzo Bogdan <bogdan.lorenzo11@gmail.com>
2019-06-17 18:24:30 +0200

    Updates

---
+++ precache-manifest.e48a226f9e73ac872e0d8f265a1fe788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41375f9be14e14a5fa448ec66f59739d",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "c901d0967976c5fa8089",
+    "url": "/web-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/web-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c901d0967976c5fa8089",
+    "url": "/web-portfolio/static/js/main.933f394f.chunk.js"
+  },
+  {
+    "revision": "4bb87171ac4011a9a17b",
+    "url": "/web-portfolio/static/js/runtime~main.790e8c02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
